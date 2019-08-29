0aafa9f0025334221aaac21e2846dd427829603a
Ravi Chandra <Ravi Chandra@LAPTOP-B9FOBDT5>
2019-08-30 00:59:02 +0530

    AddedGitHubGuideLinks

--- SingletonDesignPattern/Program.cs
+++ SingletonDesignPattern/Program.cs
@@ -2,6 +2,11 @@
 {
     class Program
     {
+        /// <summary>
+        /// Branching url https://github.com/firstcontributions/first-contributions/blob/master/github-windows-vs2017-tutorial.md
+        /// https://stackoverflow.com/questions/42217847/how-do-i-add-an-existing-solution-to-github-from-visual-studio-2017
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Singleton s1 = Singleton.GetInstance;
