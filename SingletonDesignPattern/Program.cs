7759b86f3aed11ce9047476c82fcd0fad784f87e
Ravi Chandra <Ravi Chandra@LAPTOP-B9FOBDT5>
2019-08-30 00:40:48 +0530

    Simple Single ton concept

--- SingletonDesignPattern/Program.cs
+++ SingletonDesignPattern/Program.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SingletonDesignPattern
+﻿namespace SingletonDesignPattern
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Singleton s1 = Singleton.GetInstance;
+            s1.PrintDetails("Ravi");
+            Singleton s2 = Singleton.GetInstance;
+            s2.PrintDetails("Chandra");
         }
     }
 }
---
+++ SingletonDesignPattern/SingleTon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SingletonDesignPattern
+{
+    public sealed class Singleton
+    {
+        public static int ctr = 0; 
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
+
+        /// <summary>
+        /// Public Property
+        /// </summary>
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        /// <summary>
+        /// Private Construstor 
+        /// </summary>
+        private Singleton()
+        {
+            ctr++;
+            Console.WriteLine(ctr);
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine("Print Details " + message);
+        }
+    }
+}
